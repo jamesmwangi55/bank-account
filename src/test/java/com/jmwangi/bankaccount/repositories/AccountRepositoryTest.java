9b23699897ea0058e9a27be2c749e42521581b29
jamesmwangi55 <jamesmwangi55@gmail.com>
2018-03-03 09:17:58 +0300

    Implement findByTimestampBetween test

--- src/test/java/com/jmwangi/bankaccount/repositories/AccountRepositoryTest.java
+++ src/test/java/com/jmwangi/bankaccount/repositories/AccountRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.jmwangi.bankaccount.repositories;
 
 import com.jmwangi.bankaccount.model.AccountTransaction;
+import com.jmwangi.bankaccount.utils.DateHelpers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Date;
+import java.util.List;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
@@ -40,5 +42,34 @@ public class AccountRepositoryTest {
 
         // then
         assertThat(found.getAccountNo()).isEqualTo(accountTransaction.getAccountNo());
+    }
+
+    @Test
+    public void findByTimestampBetween_thenReturnListofAccountRepositories(){
+        // given
+        AccountTransaction accountTransaction = new AccountTransaction();
+        accountTransaction.setBalance(new BigDecimal(40000).setScale(2, RoundingMode.DOWN));
+        accountTransaction.setAmount(new BigDecimal(40000).setScale(2, RoundingMode.DOWN));
+        accountTransaction.setAccountNo(2929134324L);
+        accountTransaction.setTimestamp(new Date().getTime());
+        entityManager.persist(accountTransaction);
+        entityManager.flush();
+
+        // given
+        AccountTransaction accountTransaction2 = new AccountTransaction();
+        accountTransaction2.setBalance(new BigDecimal(40000).setScale(2, RoundingMode.DOWN));
+        accountTransaction2.setAmount(new BigDecimal(40000).setScale(2, RoundingMode.DOWN));
+        accountTransaction2.setAccountNo(2929134324L);
+        accountTransaction2.setTimestamp(new Date().getTime());
+        entityManager.persist(accountTransaction2);
+        entityManager.flush();
+
+        List<AccountTransaction> transactions = accountRepository
+                .findByTimestampBetween(DateHelpers.getStartOfDay(new Date()).getTime(),
+                        DateHelpers.getEndOfDay(new Date()).getTime());
+
+        assertThat(transactions.size()).isEqualTo(2);
+        assertThat(transactions.get(0).getAccountNo())
+                .isEqualTo(accountTransaction.getAccountNo());
     }
 }
