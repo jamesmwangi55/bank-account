a4baac29426d6bf34e5f18d6dfc87e8ad437cc37
jamesmwangi55 <jamesmwangi55@gmail.com>
2018-03-03 09:25:31 +0300

    Create AccountServiceImplTest

---
+++ src/test/java/com/jmwangi/bankaccount/services/AccountServiceImplTest.java
@@ -0,0 +1,20 @@
+package com.jmwangi.bankaccount.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountServiceImplTest {
+
+    @Test
+    public void debit() {
+    }
+
+    @Test
+    public void credit() {
+    }
+
+    @Test
+    public void balance() {
+    }
+}
